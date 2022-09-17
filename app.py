928c203a59ff05d31445cb82d00c3462097621b0
MatveyKhylma <matvey@khylma.com>
2022-09-17 18:50:11 +0300

    Working web application

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import pickle
+import dill as pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-app   = Flask(__name__)
+app = Flask(__name__)
+process_new_data = pickle.load(open('process_new_data.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -13,14 +14,26 @@ def home():
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
-    print(data)
-    print(list(data.values()))
-    print(np.array(list(data.values())).reshape(1, -1))
 
-    new_data    = np.array(list(data.values())).reshape(1, -1)
+    # предобработка новых данных
+    # (перевод из dict в np.array)
+    data = list(data.values())
+    if type(data[0]) == list:
+        cols_num, lines_num = len(data), len(data[0])
+        new_data = []
+        for line in range(lines_num):
+            new_data.append([data[cols][line] for cols in range(cols_num)])
+        new_data = np.array(new_data)
+    else:
+        new_data = np.array(data)
+
+    # обработка новых данных
+    # (добавление подписей к колонкам и бинаризация
+    # соответствующих признаков)
+    new_data = process_new_data(new_data)
     predictions = model.predict(new_data)
-    print('Predictions:', predictions)
-    return jsonify(predictions)
+    print('Predictions:', predictions[0])
+    return jsonify(float(predictions[0]))
 
 if __name__ == '__main__':
     app.run(debug=True)
