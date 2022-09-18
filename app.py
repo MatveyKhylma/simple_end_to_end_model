133253fb70c8a2f0bca4113ab8ea6552a6667332
MatveyKhylma <matvey@khylma.com>
2022-09-18 19:04:26 +0300

    Added some code in home.html, updated app.py/predict_api: now it can return ALL objects

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = Flask(__name__)
 process_new_data = pickle.load(open('process_new_data.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+model            = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,6 +13,15 @@ def home():
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
+    # данная функция получает на вход данные,
+    # которые мы подали нашей модели с
+    # помощью приложения POSTMAN
+
+    # ---
+    # может обрабатывать как один объект,
+    # так и несколько
+    # ---
+
     data = request.json['data']
 
     # предобработка новых данных
@@ -32,8 +41,34 @@ def predict_api():
     # соответствующих признаков)
     new_data = process_new_data(new_data)
     predictions = model.predict(new_data)
-    print('Predictions:', predictions[0])
-    return jsonify(float(predictions[0]))
+
+    predictions_dict = {}
+    for num, pred in enumerate(predictions):
+        predictions_dict.update({'prediction_'+str(num):float(pred)})
+
+    return jsonify(predictions_dict)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # открывается веб страница, на которой можно заполнить форму
+    # и получить предсказание стоимости квартиры
+
+    # ---
+    # в отличие от реализованной выше функции
+    # в данном случае мы можем делать предсказания
+    # только для одного объекта
+    # ---
+
+    data = [float(x) for x in request.form.values()]
+    data = np.array(data)
+    new_data = process_new_data(data)
+    predictions = model.predict(new_data)
+    return render_template('home.html',
+                           prediction_text=f'The predicted price is {predictions[0]}')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
